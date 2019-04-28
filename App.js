7eff58bc62e27114e20f32203afc0bacdd68c1b2
Marcelo da Silva Sousa <marcelo.sousa1997@hotmail.com>
2019-04-28 14:42:55 -0300

    Aula 1 - Estrutura Básica de um App

--- App.js
+++ App.js
@@ -1,49 +1,18 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import React, {Component} from 'react'; //importação do react
+import { View,Text } from 'react-native'; //importação do react native
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-type Props = {};
-export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+export default class PrimeiroProjeto extends Component{
+  
+  render(){ //renderediza os componentes na tela
+    return(
+      <View>
+        <Text>Olá Mundo</Text>
+        <Text>Olá Mundo</Text>
+        <Text>Olá Mundo</Text>
+        <Text>Olá Mundo</Text>
       </View>
     );
   }
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+
+}
