5eb1128a2b83e14e5a5ae82140dc340bff6c24e4
marcelo sousa <marcelo.sousa1997@hotmail.com>
2019-04-28 15:46:38 -0300

    Aula 2 - Detalhes sobre ES6

--- App.js
+++ App.js
@@ -1,18 +1,31 @@
 import React, {Component} from 'react'; //importação do react
-import { View,Text } from 'react-native'; //importação do react native
+import { View,Text,StyleSheet,Button } from 'react-native'; //importação do react native
 
 export default class PrimeiroProjeto extends Component{
-  
+
+  somar(n1,n2){
+    return n1+n2;
+  }
   render(){ //renderediza os componentes na tela
+
+    let nome = 'Marcelo';
     return(
       <View>
+        <Text>Olá Mundo {nome}</Text>
         <Text>Olá Mundo</Text>
         <Text>Olá Mundo</Text>
-        <Text>Olá Mundo</Text>
-        <Text>Olá Mundo</Text>
+        <Text style={style.texto}>A soma de 2+2 é {this.somar(2,2)}</Text>
+        <Button title="Aperte" onPress={()=>{
+          alert("Me apertou");
+        }} />
+        <Button title="Aperte Novamente" onPress={()=> alert("Me apertou novamente")}/>
       </View>
     );
   }
-
-
 }
+
+const style = StyleSheet.create({
+  texto:{
+    fontSize:30
+  }
+})
