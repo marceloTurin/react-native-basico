c7bedba64786855a0607594bf4f3c52afe92b237
marcelo sousa <marcelo.sousa1997@hotmail.com>
2019-05-01 14:48:01 -0300

    Aula 9 - Recebendo dados do usuário

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react'; //importação do react
-import { View,Text,StyleSheet,Button,Image } from 'react-native'; //importação do react native
+import { View,Text,StyleSheet,Button,Image,TextInput } from 'react-native'; //importação do react native
 
 
 //Criação do Componente Imagem
@@ -47,21 +47,40 @@ class Janta extends Component{
 
 export default class PrimeiroProjeto extends Component{
 
+  constructor(props){
+    super(props)
+    this.state = {texto:''}
+    this.mudarTexto = this.mudarTexto.bind(this)
+  }
+
+  mudarTexto(t){
+    let s = this.state;
+    if(t.length > 0){
+      s.texto = `Olá,${t}`;
+    }
+    else{
+      s.texto = '';
+    }
+    this.setState(s);
+
+
+    
+  }
  
   render(){ //renderiza os componentes na tela
 
    
     return(
-       <View style={{flex:1,flexDirection:'row',justifyContent:'center',alignItems:'center'}}>
-        <View style={{width:50, height:50,backgroundColor:'blue'}}></View>
-        <View style={{width:50, height:50,backgroundColor:'green'}}></View>
-        <View style={{width:50, height:50, backgroundColor:'yellow'}}></View>
+      <View style={{paddingTop:30}}>
+        <TextInput style={styles.input} placeholder="Qual o seu nome?" onChangeText={this.mudarTexto} />
+        <Text style={styles.texto}>{this.state.texto}</Text>
       </View>
+
     );
   }
 }
 
-const style = StyleSheet.create({
+const styles = StyleSheet.create({
 
   padrao:{
     paddingTop:25,
@@ -75,5 +94,18 @@ const style = StyleSheet.create({
   vermelho:{
     color:'red',
     fontSize:15
+  },
+  input:{
+    height:40,
+    borderWidth:1,
+    borderColor: '#000000',
+    margin:10,
+    padding:10
+  },
+  texto:{
+    fontSize:20,
+    textAlign:'center'
   }
+
+
 })
